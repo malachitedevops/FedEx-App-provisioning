18c206af7c4e35a54b7d048b9f4486e931a59ba0
Mark Hegedus <hmark33@gmail.com>
2019-08-01 14:02:43 +0200

    fix(getdependencies.sh): add docker privileges to selected users

--- getdependencies.sh
+++ getdependencies.sh
@@ -4,5 +4,8 @@ sudo apt install nodejs -y
 sudo apt install npm -y
 sudo npm install forever -g
 bash /tmp/provisioning/getdocker.sh
+sudo groupadd docker
+sudo usermod -aG docker ubuntu
+sudo usermod -aG docker sysadmin
 sudo apt update -y && sudo apt upgrade -y && sudo apt full-upgrade -y && sudo apt clean -y && sudo apt autoremove -y
 
