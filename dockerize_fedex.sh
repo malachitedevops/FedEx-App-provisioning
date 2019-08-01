4fcad6a7d4674c4ecf1df4183e729beb9f14b278
Mark Hegedus <hmark33@gmail.com>
2019-08-01 20:06:13 +0200

    feat(dockerize_fedex.sh): add script

---
+++ dockerize_fedex.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sleep 120
+cd /home/ubuntu/app/docker && sudo docker pull hmarks/fedexday:latest
+
+docker kill my_fedexday && docker rm my_fedexday || echo "No such docker container"
+
+docker run --name my_fedexday -p 3000:3000 -p 4200:4200 -d hmarks/fedexday
+docker cp /home/ubuntu/app/appenv/.env my_fedexday:/usr/src/app/
+docker restart my_fedexday
