f9a14228d6ce6badfb7fac8d967ce3e37a1b3e73
Mark Hegedus <hmark33@gmail.com>
2019-08-13 16:41:21 +0200

    feat(dockerize_fedex.sh): delete environmentals after starting docker image

--- dockerize_fedex.sh
+++ dockerize_fedex.sh
@@ -8,3 +8,5 @@ docker kill my_fedexday && docker rm my_fedexday || echo "No such docker contain
 docker run --name my_fedexday -p 3000:3000 -p 4200:4200 -d hmarks/fedexday
 docker cp /home/ubuntu/app/appenv/.env my_fedexday:/usr/src/app/
 docker restart my_fedexday
+
+rm /home/ubuntu/app/appenv/.env
